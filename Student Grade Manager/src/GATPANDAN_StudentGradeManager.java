b5f104995be453b8fd1858a41db4d15be0954be8
Jerico Gatpandan <jerico.gatpandan@unc.edu.ph>
2025-02-06 01:48:42 +0800

    Fix the formatting of average and add some comments

--- Student Grade Manager/src/GATPANDAN_StudentGradeManager.java
+++ Student Grade Manager/src/GATPANDAN_StudentGradeManager.java
@@ -50,7 +50,7 @@ public class GATPANDAN_StudentGradeManager {
 				System.out.print("Enter Name: ");
 				String name = input.nextLine();
 
-				// Prompt the user to enter the grades
+				// Input grades
 				String[] gradeTypes = { "quiz", "activity", "exam" };
 				int[] grades = new int[3];
 				for (int i = 0; i < gradeTypes.length; i++) {
@@ -60,18 +60,22 @@ public class GATPANDAN_StudentGradeManager {
 
 				Student student = new Student(name, grades[0], grades[1], grades[2]);
 
+				// Update pass/fail count
 				if (student.hasPassed()) {
 					passedStudent++;
 				} else {
 					failedStudents++;
 				}
 
-				printWriter.println(student.name + "," + student.quizGrade + "," + student.activityGrade + ","
-						+ student.examGrade + "," + student.average);
+				// Append to file
+				printWriter.print(student.name + "," + student.quizGrade + "," + student.activityGrade + ","
+						+ student.examGrade + "," + String.format("%.1f", student.average) + "\n");
 
+				// Display result
 				System.out.println("\nNumber of Passed: " + passedStudent);
 				System.out.println("Number of Failed: " + failedStudents);
 
+				// Ask user to continue
 				System.out.print("\nDo you want to enter again? (Yes/No) ");
 				String choice = input.nextLine().trim();
 				if (choice.equalsIgnoreCase("No")) {
